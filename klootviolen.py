af801c1cb52ed01d510cb7db9b6608ec4d41ac54
Adirondac0 <32670569+Adirondac0@users.noreply.github.com>
2018-03-24 12:46:24 +0100

    eerste programma ooit!

---
+++ klootviolen.py
@@ -0,0 +1,34 @@
+# beller = "opa"
+
+# # # Deze if en else statement doen wat als 
+# # if(beller == "kindje" or beller == "opa"):
+# # 	print("geef mama maar even")
+# # else:
+# # 	print("els!!!")
+
+
+# while(beller == "opa"):
+# 	print("els!!!")
+
+# teller = 0
+
+# for teller in range(100):
+# 	teller += 20
+# 	print(teller)
+
+# teller = 0 
+
+# while(teller < 10000):
+# 	teller *= 20 # is het zelfde als zeggen teller = teller + 20
+# 	print(teller)
+
+
+
+# this function checks if a  number is even or odd
+
+number = 9
+
+if(number%3 == 0):
+	print("the number " + str(number) + " can be divided by 3")
+else:
+	print("the number " + str(number) + " cannot be dived by 3")
